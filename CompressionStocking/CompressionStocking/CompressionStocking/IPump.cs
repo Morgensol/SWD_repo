9046b3c4e65e744cd22d2dfc048dd70ec5b4ef62
William <williamspost@hotmail.com>
2019-05-01 19:46:42 +0200

    cleanup

---
+++ CompressionStocking/CompressionStocking/CompressionStocking/IPump.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompressionStocking
+{
+    interface IPump
+    {
+        void runForwards(int seconds);
+        void runBackwards(int seconds);
+        void stop();
+    }
+}
---
+++ CompressionStocking/CompressionStocking/CompressionStocking/Pump.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+namespace CompressionStocking
+{
+    class Pump: IPump
+    {
+        public void runForwards(int seconds)
+        {
+            var tmr = new Timer();
+            tmr.
+        }
+
+        public void runBackwards(int seconds)
+        {
+            
+        }
+
+        public void stop()
+        {
+
+        }
+    }
+}
